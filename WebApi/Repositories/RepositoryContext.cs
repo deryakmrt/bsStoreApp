e8bf899b02c23a9ca027548f6fd6750b18babbba
Derya Kımırtı <96728812+deryakmrt@users.noreply.github.com>
2025-10-21 08:47:42 +0300

    new

---
+++ WebApi/Repositories/RepositoryContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Repositories
+{
+    //"Benim 'Books' adında bir tablom olacak ve bu tablo Book nesnelerinden oluşacak"
+    //gemini geçmişine bakıp kodun açıklamasını yaz!
+    //github a bu projeyi ekle
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) 
+            : base(options) { }
+        public DbSet <Book> Books { get; set; }
+    }
+}
