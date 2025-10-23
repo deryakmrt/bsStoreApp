a564a3327a212891d309f431308be067d09c6281
Derya Kımırtı <96728812+deryakmrt@users.noreply.github.com>
2025-10-23 17:55:19 +0300

    Update Program.cs
    IoC için tanımlama yapıldı

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//bir dbcontexte ihtiyacýmýz olduðunda bunun somut haline izin verecek demek, veritabanýnda sorun yaþanmayacak
+//burasý reposityContext(depo müdürü) tanýmý/tarifi yapýlan yer. Bunu IoC'ye (ÝK'ya) teslim ederim. 
+builder.Services.AddDbContext<RepositoryContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection")));
 
 var app = builder.Build();
 
